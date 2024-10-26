0d387e21ab59e5fb0986e935134195737141b835
ArghyaBanerjee30 <arghya.banerjee@thoughtworks.com>
2024-10-27 02:27:55 +0530

    Add. docstrings

--- src/pipeline/run.py
+++ src/pipeline/run.py
@@ -6,10 +6,18 @@ from src.pipeline.model.config import Config
 from src.pipeline.service.config_service import ConfigService
 
 async def run_task(config: Config):
+    """
+    Creates a Loader instance with the provided ingestion configuration
+    and initiates the data ingestion process.
+    """
     loader = Loader(config.ingestion_config)
     await loader.run()
 
 async def run(file_path:str):
+    """
+    Loads configuration from the specified file path, retrieves the config data,
+    and starts the task using the configuration provided.
+    """
     config = ConfigService(file_path).get_config()
     await run_task(config)
 
--- src/pipeline/service/config_service.py
+++ src/pipeline/service/config_service.py
@@ -7,11 +7,30 @@ from src.pipeline.model.config import Config
 
 
 class ConfigService:
+    """
+    Service class responsible for loading and parsing configuration data from a YAML file.
+    """
     def __init__(self, file_path: str):
+        """
+        Initializes ConfigService with a file path for the configuration YAML file.
+
+        Parameters:
+        - file_path (str): The path to the configuration YAML file.
+        """
         load_dotenv()
         self.path = file_path
 
     def __load_config(self) -> dict:
+        """
+        Loads and processes the YAML configuration file, expanding environment variables.
+
+        Returns:
+        - dict: The processed configuration data as a dictionary.
+
+        Raises:
+        - FileNotFoundError: If the specified file path does not exist.
+        - yaml.YAMLError: If the YAML file is improperly formatted.
+        """
         with open(self.path, "r") as file:
             raw_config = file.read()
 
@@ -19,6 +38,15 @@ class ConfigService:
         return yaml.safe_load(processed_config)
 
     def get_config(self) -> Config:
+        """
+        Retrieves the parsed configuration as a Config object.
+
+        Returns:
+        - Config: A Config object containing ingestion configurations.
+
+        Raises:
+        - ValueError: If the 'ingestion' section is missing in the configuration data.
+        """
         config_data = self.__load_config()
 
         if "ingestion" not in config_data:
