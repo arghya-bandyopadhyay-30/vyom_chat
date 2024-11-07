01970bf8d22f2f865d5abee5b00a35e141d65f08
ArghyaBanerjee30 <arghya.banerjee@thoughtworks.com>
2024-11-07 12:33:26 +0530

    [Arghya] Refactor. prompt manager class

--- src/llm/service/llm_service.py
+++ src/llm/service/llm_service.py
@@ -4,7 +4,7 @@ from langchain_groq import ChatGroq
 from src.embedding.models.embedding import Embedding
 from src.embedding.service.embedding_service import EmbeddingService
 from src.llm.model.llm import LLMConfig
-from src.llm.utilities.prompt import get_agent_prompt
+from src.llm.utilities.prompt import PromptManager
 
 
 class LLMService:
@@ -13,9 +13,10 @@ class LLMService:
         self.groq_api_key = llm_config.api_key
         self.model_name = llm_config.model
 
-        self.embedding_service = EmbeddingService(embedding_config)
-
         self.llm = ChatGroq(groq_api_key=self.groq_api_key, model_name=self.model_name)
+
+        self.embedding_service = EmbeddingService(embedding_config)
+        self.prompt_manager = PromptManager(self.embedding_service)
 
     async def query(self, text: str):
         prompts = self.__generate_prompt(text)
@@ -23,4 +24,4 @@ class LLMService:
         return response.generations[0][0].text
 
     def __generate_prompt(self, text: str) -> list:
-        return get_agent_prompt(text, self.embedding_service)
+        return self.prompt_manager.get_agent_prompt(text)
