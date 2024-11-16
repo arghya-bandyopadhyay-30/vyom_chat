59c1a15ead3c047f64493c2efb13103edcc6513a
ArghyaBanerjee30 <arghya.banerjee@thoughtworks.com>
2024-11-16 10:07:54 +0530

    [Arghya] refactor. app file

--- src/app/app.py
+++ src/app/app.py
@@ -4,15 +4,12 @@ from src.app.models.app_config import AppConfig
 from src.app.service.app_service import AppService
 from src.app.webapp import WebApp
 
+def run(file_path:str):
+    config = AppService(file_path).get_config()
 
-async def run_task(config: AppConfig):
     web = WebApp(config.app_config, config.llm_config)
     web.run()
 
-async def run(file_path:str):
-    config = AppService(file_path).get_config()
-    await run_task(config)
-
 if __name__ == "__main__":
     app_config_path = "app.yml"
-    asyncio.run(run(app_config_path))
+    run(app_config_path)
